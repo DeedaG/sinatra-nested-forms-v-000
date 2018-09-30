b6e546ce4a10fbe0a342067c77a2376348e51db6
Christia T <christiatidwell@bellsouth.net>
2018-09-30 23:51:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,32 +3,5 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    get '/pirates' do
-      "Welcome to the Nested Forms Lab!  let's navigate to the '/new'"
-      erb :new
-    end
-
-    get '/pirates/new' do
-      erb :pirates/new
-    end
-
-    get '/pirates/:id' do
-      @pirate = Pirate.get(params[:id])
-
-      erb :'pirates/show'
-    end
-
-
-    post '/pirates' do
-      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:height], params[:pirate][:weight])
-      params[:pirate][:ships].each do |ship_data|
-        ship = Ship.new(ship_data)
-        ship.pirate = pirate
-        ship.save
-      end
-
-      redirect to "/pirates/#{pirate.id}"
-      #please go to localhost:9393/pirates/this pirates id
-    end
   end
 end
