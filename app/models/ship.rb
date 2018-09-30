a7d7866a62a8094589a749be1bac1bde8857477c
Christia T <christiatidwell@bellsouth.net>
2018-09-30 23:32:12 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -4,9 +4,10 @@ attr_reader :name, :type, :booty
 
 SHIPS = []
 
-def initialize(args)
-  @name =
-  @type =
-  @booty =
-  SHIPS << self
+  def initialize(args)
+    @name =
+    @type =
+    @booty =
+    SHIPS << self
+  end
 end
