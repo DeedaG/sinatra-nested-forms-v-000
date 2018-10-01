142180ce2e0ebe112aac2538a05bb59e45b83fff
Christia T <christiatidwell@bellsouth.net>
2018-10-01 00:43:46 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,10 +4,10 @@ attr_reader :name, :weight, :height
 
 PIRATES = []
 
-  def initialize(params)
-    @name = params[:name]
-    @weight = params[:weight]
-    @height = params[:height]
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
     PIRATES << self
   end
 
