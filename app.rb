7b877ed54fded6a377778bcbfe0eeebdf368d23a
Christia T <christiatidwell@bellsouth.net>
2018-10-01 00:37:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,10 +14,7 @@ module FormsLab
     post '/pirates' do
       @pirate= Pirate.new(params[:pirate][:name], params[:pirate][:height], params[:pirate][:weight])
       params[:pirate][:ships].each do |ship|
-
-        name
-        type
-        booty
+        Ship.new(ship[:name], ship[:type], ship[:booty])
       end
       @ships = Ship.all
       erb :'pirates/show'
